4514588b0250a566baaf16a90d36d9a37d1e3ddf
joshunrau <contact@joshuaunrau.com>
2023-05-17 10:45:00 -0400

    add css

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
 
 import pkg from './package.json' assert { type: 'json' };
 
@@ -20,11 +21,26 @@ export default [
         sourcemap: true
       }
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })]
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss({
+        config: {
+          path: './postcss.config.js'
+        },
+        extensions: ['.css'],
+        minimize: true,
+        inject: {
+          insertAt: 'top'
+        }
+      })
+    ]
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()]
+    plugins: [dts()],
+    external: [/\.css$/]
   }
 ];
