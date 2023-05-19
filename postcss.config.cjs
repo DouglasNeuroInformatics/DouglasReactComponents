42193651a56b31755384dfa46632a8391dd61a97
joshunrau <contact@joshuaunrau.com>
2023-05-19 13:59:18 -0400

    actually can use esm

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-const autoprefixer = require('autoprefixer');
-const tailwindcss = require('tailwindcss');
-
-/** @type {import('postcss-load-config').Config} */
-module.exports = {
-  plugins: [tailwindcss, autoprefixer]
-};
