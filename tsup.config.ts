873b76e0c50552767454cd1bcfd2bb646e9da41e
joshunrau <contact@joshuaunrau.com>
2023-05-24 23:58:10 -0400

    add use client

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,9 +4,15 @@ export default defineConfig({
   clean: true,
   dts: true,
   entry: ['src/index.ts'],
+  esbuildOptions: (options) => {
+    options.banner = {
+      js: '"use client";'
+    };
+  },
   external: ['react', 'react-dom'],
   format: ['esm', 'cjs'],
   minify: true,
   sourcemap: true,
+  splitting: false,
   target: 'es2018'
 });
