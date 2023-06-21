0f8a29e776d6e88108c47e0148925d86dbedad19
joshunrau <contact@joshuaunrau.com>
2023-06-21 13:09:13 -0400

    redarken

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -25,7 +25,7 @@ export const Button = React.forwardRef(function Button(
           'px-4 py-2 text-sm': size === 'sm',
           'text-md px-6 py-2': size === 'md',
           'px-8 py-3 text-lg': size === 'lg',
-          'bg-slate-700 text-slate-100 hover:bg-slate-600 dark:bg-sky-700 dark:hover:bg-sky-600':
+          'bg-slate-800 text-slate-100 hover:bg-slate-700 dark:bg-sky-700 dark:hover:bg-sky-600':
             variant === 'primary',
           'border bg-inherit': variant === 'secondary',
           'bg-red-600 text-white hover:bg-opacity-90': variant === 'danger'
